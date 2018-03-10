972c6074ef5d1aafc45d1ccb2520caf51385827e
onebone <jyc00410@gmail.com>
2018-03-11 03:09:03 +0900

    Initial commit

---
+++ util.go
@@ -0,0 +1,57 @@
+package onessentials
+
+import (
+	"os"
+	"io"
+	"path"
+	"path/filepath"
+	"io/ioutil"
+	"encoding/json"
+)
+
+
+func CopyFile(path, dest string) error {
+	in, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+
+	e := out.Close()
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func InitConfig(config interface{}) (err error) {
+	var base string
+	base, err = filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return
+	}
+
+	if _, err = os.Stat(path.Join(base, "config.json")); os.IsNotExist(err) {
+		CopyFile(path.Join(base, "resources", "config.json"), path.Join(base, "config.json"))
+	}
+
+	b, err := ioutil.ReadFile(path.Join(base, "config.json"))
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(b, &config)
+	return
+}
